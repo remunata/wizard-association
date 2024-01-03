42d4d9131b2932c49097884b9056d8f6d26018ad
Mahendra Dinata <remdinata@gmail.com>
2024-01-03 22:33:10 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <div className="flex items-center h-screen">
+          <h1 className="text-8xl font-bold">Wizard Association</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
